f0718d7bec39405a138d09b7ee9d0ce07a3f626c
Pandemoniusly <157427865+Pandemoniusly@users.noreply.github.com>
2026-02-27 14:40:02 -0500

    Add project files.

---
+++ FraudTweaks.cs
@@ -0,0 +1,46 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FraudTweaks
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class FraudTweaks : BaseUnityPlugin
+    {
+        public static FraudTweaks Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger { get; private set; } = null!;
+        internal static Harmony? Harmony { get; set; }
+        public static float RocketDelay = 0f;
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            Patch();
+
+            Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+        }
+
+        internal static void Patch()
+        {
+            Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            Logger.LogDebug("Patching...");
+
+            Harmony.PatchAll();
+
+            Logger.LogDebug("Finished patching!");
+        }
+
+        internal static void Unpatch()
+        {
+            Logger.LogDebug("Unpatching...");
+
+            Harmony?.UnpatchSelf();
+
+            Logger.LogDebug("Finished unpatching!");
+        }
+    }
+}
---
+++ Patches/RocketPatch.cs
@@ -0,0 +1,71 @@
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace FraudTweaks.Patches
+{
+    [HarmonyPatch(typeof(Grenade))]
+    public class Rocket
+    {
+        [HarmonyPatch("PlayerRideStart")]
+        [HarmonyPostfix]
+        private static void RideOn(Grenade __instance, ref GameObject ___interruptSphere)
+        {
+            if (___interruptSphere != null)
+            {
+                ___interruptSphere.SetActive(true);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ShotgunHammer))]
+    public class JackhammerRocket
+    {
+        [HarmonyPatch("ImpactRoutine")]
+        [HarmonyPrefix]
+        private static void RocketPuncher(ShotgunHammer __instance)
+        {
+            Vector3 position = MonoSingleton<CameraController>.Instance.GetDefaultPos();
+            Collider[] cols = Physics.OverlapSphere(position, 0.01f);
+            if (cols.Length != 0)
+            {
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    Transform transform = cols[i].transform;
+                    if (transform.TryGetComponent<ParryHelper>(out var component))
+                    {
+                        transform = component.target;
+                    }
+                    if (MonoSingleton<ObjectTracker>.Instance.grenadeList.Count > 0 && transform.gameObject.layer == 10)
+                    {
+                        Grenade componentInParent = transform.GetComponentInParent<Grenade>();
+                        if ((bool)componentInParent & componentInParent.rocket)
+                        {
+                            if (FraudTweaks.RocketDelay > 4f) 
+                            {
+                                GameObject interrupt = AccessTools.Field(typeof(Grenade), "interruptSphere").GetValue(componentInParent) as GameObject;
+                                interrupt.SetActive(false);
+                            }
+                            else
+                            {
+                                GameObject interrupt = AccessTools.Field(typeof(Grenade), "interruptSphere").GetValue(componentInParent) as GameObject;
+                                interrupt.SetActive(true);
+                                FraudTweaks.RocketDelay = FraudTweaks.RocketDelay + 1;
+                                if (componentInParent.levelledUp) FraudTweaks.RocketDelay = FraudTweaks.RocketDelay + 0.5f;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        private static void DelayFallOff(ShotgunHammer __instance)
+        {
+            FraudTweaks.RocketDelay = Mathf.Clamp(FraudTweaks.RocketDelay - Time.deltaTime, 0, 5);
+        }
+    }
+}
