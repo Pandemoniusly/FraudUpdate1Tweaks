ffe970a6452777fa0c5f7e857803d33c69bd0740
Pandemoniusly <157427865+Pandemoniusly@users.noreply.github.com>
2026-02-28 19:33:43 -0500

    Final coin fix

--- Patches/SplitShotFix.cs
+++ Patches/SplitShotFix.cs
@@ -24,9 +24,9 @@ namespace FraudTweaks.Patches
         private static void RevolverPierce(RevolverBeam __instance,ref PhysicsCastResult rayHit, ref bool ___splitcoinable, ref LayerMask ___enemyLayerMask)
         {
             bool setSplit = false;
-            if (!__instance.strongAlt)
+            if (!__instance.strongAlt & __instance.name.Contains("Revolver Beam Super"))
             {
-                ___splitcoinable = true;
+                ___splitcoinable = false;
                 setSplit = true;
             }
             if (setSplit || ___splitcoinable)
@@ -39,17 +39,36 @@ namespace FraudTweaks.Patches
                 Vector3 endPoint;
                 PhysicsCastResult hitInfo;
                 bool flag2 = PortalPhysicsV2.Raycast(position, forward, num2, ___enemyLayerMask, out hitInfo, out portalTraversals, out endPoint);
-                if (flag2 & hitInfo.transform.gameObject.TryGetComponent<Coin>(out var coin))
+                if (flag2)
                 {
-                    Vision coinVision = AccessTools.Field(typeof(Coin), "vision").GetValue(coin) as Vision;
+                    if (hitInfo.transform.gameObject.TryGetComponent<Coin>(out var coin))
+                    {
+                        Vision vision = AccessTools.Field(typeof(Coin), "vision").GetValue(coin) as Vision;
                         VisionQuery enemyQuery = AccessTools.Field(typeof(Coin), "enemyQuery").GetValue(coin) as VisionQuery;
-                        coinVision.UpdateSourcePos(coin.transform.position);
+                        VisionQuery coinQuery = AccessTools.Field(typeof(Coin), "coinQuery").GetValue(coin) as VisionQuery;
+                        vision.UpdateSourcePos(coin.transform.position);
                         TargetDataRef Data;
-                        bool CanSee = coinVision.TrySee(enemyQuery, out Data);
-                        if (!CanSee || coinVision.visionIndex == -1)
+                        TargetDataRef Data2;
+                        bool CanSeeEnemy = vision.TrySee(enemyQuery, out Data);
+                        bool CanSeeCoin = vision.TrySee(coinQuery, out Data2);
+                        if (CanSeeCoin)
                         {
-                            ___splitcoinable = false;
+                            if (Data2.target.GameObject.TryGetComponent<Coin>(out var coin2))
+                            {
+                                if (coin2.ccc != null)
+                                {
+                                    if (coin2.ccc.beenHit.Contains(coin2.gameObject))
+                                    {
+                                        ___splitcoinable = true;
+                                    }
+                                }
+                            }
                         }
+                        if (CanSeeEnemy & !CanSeeCoin)
+                        {
+                            ___splitcoinable = true;
+                        }
+                    }
                 }
             }
         }
